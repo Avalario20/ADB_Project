f9456054fea4b478ef48c5a054fdf7052719ae08
Avalario20 <104756549+Avalario20@users.noreply.github.com>
2024-05-24 22:14:15 +0200

    Update entete.php

--- Cirlincione_Luano_ProjetADB/ProjetADB/entete.php
+++ Cirlincione_Luano_ProjetADB/ProjetADB/entete.php
@@ -12,10 +12,10 @@ session_start();
 <div class = "header">
     <img src='/ProjetADB/images/logo_snd.png' alt="logo" class="logo">
     <div class = "navbar">
+        <?php if(isset($_SESSION['auth'])){?>
         <li class = "nav"><a href="accueil.php" class ="btnnav">Accueil</a></li>
         <li class = "nav"><a href="userquestion.php" class ="btnnav">Mes Questions</a></li>
         <li class = "nav"><a href="publishthread.php" class ="btnnav">Publier</a></li>
-        <?php if(isset($_SESSION['auth'])){?>
         <li class = "nav"><a href="logout.php" class ="btnnav">Déconnexion</a></li>
         <?php }?>
     </div>
